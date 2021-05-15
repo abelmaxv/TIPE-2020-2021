ad395ae664e0d6edc3f115ff6589f46ba1536518
abelmaxv <59065710+abelmaxv@users.noreply.github.com>
2021-05-15 15:45:52 +0200

    Update crible_eratosthene.py

--- crible_eratosthene.py
+++ crible_eratosthene.py
@@ -3,7 +3,7 @@ from math import isqrt
 #Crible d'Eratosthène
 
 def eratosthene(n):
-    if n <= 2:
+    if n < 2:
         return []
     is_prime = [True]*n
     is_prime[0],is_prime[1] = False, False
@@ -12,5 +12,6 @@ def eratosthene(n):
         if is_prime[i]:
             for x in range(i**2, n, i):
                 is_prime[x]= False
+    
     return [i for i in range(n) if is_prime[i]]
     
